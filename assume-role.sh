3a357728a0cc8cc3663c31282f695776025f9795
nttd-shigemi <hodaka.shigemi@fastretailing.com>
2018-09-14 15:04:31 +0900

    fix exit message

--- assume-role.sh
+++ assume-role.sh
@@ -1,5 +1,5 @@
 if [ -f $0 ];then
-    echo "Usage: \"source $0\"  or \" . $0\"" >&2
+    echo "Usage: \"source $0 PROFILE_NAME\"  or \" . $0 PROFILE_NAME\"" >&2
     exit 1
 else
     eval $(~/.aws/print_aws_credentials.py $1)
