063851a8ac855ce9edff1590fbbba3350641196b
HodakaShigemi <shigemi.hodaka@simpline.co.jp>
2021-12-28 11:30:59 +0900

    Update assume-role.sh

--- assume-role.sh
+++ assume-role.sh
@@ -2,5 +2,5 @@ if [ -f $0 ];then
     echo "Usage: \"source $0 PROFILE_NAME\"  or \" . $0 PROFILE_NAME\"" >&2
     exit 1
 else
-    eval $(~/.aws/print_aws_credentials.py $1)
+        eval $( ~/awsutil/print_aws_credentials.py $1)
 fi
